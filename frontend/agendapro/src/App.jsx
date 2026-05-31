7659ddfdb92ef976a54d7c24519f688a363cea79
Kaikyterto <kaikyterto9@gmail.com>
2026-05-31 13:16:30 -0300

    fixing admin seetings

--- frontend/agendapro/src/App.jsx
+++ frontend/agendapro/src/App.jsx
@@ -14,6 +14,7 @@ import AdminBookingPage from "./pages/AdminBookingPage";
 import AdminProductsPage from "./pages/AdminProductsPage";
 import AdminWorkersPage from "./pages/AdminWorkersPage";
 import AdminDashboardPage from "./pages/AdminDashboardPage";
+import AdminSettingsPage from "./pages/AdminSettingsPage";
 
 export default function App() {
   return (
